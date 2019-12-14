a34e211438e1b8cd9630668d26bdde1d59c72801
Fraser <tomatoesneverlie@gmail.com>
2019-12-14 15:04:41 -0700

    Turning Turrret

---
+++ Assets/Scripts/Components/TurretManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretManager : MonoBehaviour
+{
+	private GameObject[] units;
+	public float turnSpeed = 3f;
+	void Start()
+	{
+		units = GameObject.FindGameObjectsWithTag("Unit");
+	}
+
+	void Update()
+	{
+		var direction = units[0].transform.position-transform.position;
+ 		direction.y = 0;
+ 		transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime*turnSpeed);
+	}
+}
