9d95d65036b05d0e00792739c383b25273d15971
Fraser <tomatoesneverlie@gmail.com>
2019-12-14 15:42:22 -0700

    Fireing

--- Assets/Scripts/Components/TurretManager.cs
+++ Assets/Scripts/Components/TurretManager.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class TurretManager : MonoBehaviour
 {
+	public float turnSpeed = 40;
+	public float radius = 12;
+	public float bulletSpeed = 6;
+	public float fireRate = 1;
+	public GameObject bullet;
+	public Transform firePoint;
 	private GameObject[] units;
-	public float turnSpeed = 3f;
 	void Start()
 	{
 		units = GameObject.FindGameObjectsWithTag("Unit");
 	}
-
+	private int target = -1;
+	private float charge = 0;
 	void Update()
 	{
-		var direction = units[0].transform.position-transform.position;
- 		direction.y = 0;
- 		transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime*turnSpeed);
+		if(target == -1||(transform.position-units[target].transform.position).magnitude>radius){
+			target = - 1;
+			for(int i = 0; i < units.Length; i++){
+				if((transform.position-units[i].transform.position).magnitude<radius){
+					target = i;
+					break;
+				}
+			}
+		}
+		if(target!=-1){
+			var direction = units[target].transform.position-transform.position;
+			direction.y = 0;
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime*turnSpeed);
+			
+			charge += Time.deltaTime;
+			if(charge > 1/fireRate){
+				charge = 0;
+				var bulletInstance = Instantiate(bullet, firePoint.position, firePoint.rotation);
+				bulletInstance.GetComponent<Rigidbody>().velocity = bulletSpeed*bulletInstance.transform.forward;
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/bulletManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletManager : MonoBehaviour
+{
+	public float range = 25;
+	private Vector3 src;
+
+	void Start(){
+		src = transform.position;
+	}
+	void Update()
+	{
+		if((transform.position-src).magnitude>range)GameObject.Destroy(gameObject);
+	}
+}
